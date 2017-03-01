d96cd1484b1e5a2550ec20eeb1db5c8c112b05e0
Ken Aizawa <k.aizawa@live.com>
2017-03-01 02:42:20 +0000

    cleanup

--- data_preprocess.py
+++ data_preprocess.py
@@ -14,7 +14,7 @@
 # http://opus.lingfil.uu.se/download.php?f=OpenSubtitles2016/mono/OpenSubtitles2016.raw.en.gz
 
 # French movie subtitles
-# http://opus.lingfil.uu.se/download.php?f=OpenSubtitles2016/mono/OpenSubtitles2016.raw.fr.gz   
+# http://opus.lingfil.uu.se/download.php?f=OpenSubtitles2016/mono/OpenSubtitles2016.raw.fr.gz
 
 import numpy as np
 import os
@@ -25,36 +25,45 @@ import urllib.request
 import gzip
 import math
 
+# Count tokens and assign id's for each unique token. Use token counts to
+# exclude rare tokens. Save one-hot token id's.
+
+# file_path = subtitles.gz file path
+# write_path = write path for dictionary of token id's
+# num_batches = number of batches to split data parsing
+# min_count = min number of tokens
+
+
 def read_data(file_path, write_path, num_batches, min_count):
-    
+
     with gzip.open(file_path, 'rb') as f:
-            content = f.read()
-    
+        content = f.read()
+
     tok_counts = collections.Counter()
 
     for i in range(0, num_batches):
         start = math.floor(i * len(content) / num_batches)
         end = math.floor((i + 1) * len(content) / num_batches)
-    
+
         toks = re.findall(r"[\w]+|[^\s\w]", content[start:end].decode('utf-8'))
 
         for tok in toks:
-            tok_counts[tok]+=1
-    
+            tok_counts[tok] += 1
+
     print("...tokens counted")
 
     onehot_tok_idx = {}
-    
-    tok_idx=0
+
+    tok_idx = 0
 
     for i in range(0, num_batches):
-        
+
         start = math.floor(i * len(content) / num_batches)
         end = math.floor((i + 1) * len(content) / num_batches)
 
         sent = content[start:end].decode('utf-8').split('\n')
-        
-        print("...subtitles in memory for batch %d/%d"%(i+1, num_batches))
+
+        print("...subtitles in memory for batch %d/%d" % (i + 1, num_batches))
 
         for sent_idx in range(0, len(sent)):
 
@@ -62,17 +71,27 @@ def read_data(file_path, write_path, num_batches, min_count):
 
             for tok in sent_tok:
                 if tok_counts[tok] < min_count:
-                   tok = "UNKNOWNTEXT"
+                    tok = "UNKNOWNTEXT"
                 if tok not in onehot_tok_idx:
-                    onehot_tok_idx[tok]=tok_idx
-                    tok_idx+=1
+                    onehot_tok_idx[tok] = tok_idx
+                    tok_idx += 1
 
         del sent
 
-        print("...token id's assigned for batch %d/%d"%(i+1,num_batches))
+        print("...token id's assigned for batch %d/%d" % (i + 1, num_batches))
 
     np.save(write_path, onehot_tok_idx)
     print("...token id's saved")
+
+# Loop through batches and generate one-hot encodings of sequences and
+# sequence labels.
+
+# file_path = subtitles.gz file path
+# onehot_tok_idx = dictionary of token id's
+# num_batches = number of batches to split data parsing
+# batch_size = size of batches to generate
+# seq_length = max sequence length
+
 
 def data_iterator(file_path, onehot_tok_idx, num_batches, batch_size, seq_length):
 
@@ -87,12 +106,14 @@ def data_iterator(file_path, onehot_tok_idx, num_batches, batch_size, seq_length
 
             sent = content[start:end].decode('utf-8').split('\n')
 
-            onehot_seq_batch = np.empty(shape=(batch_size, seq_length, len(onehot_tok_idx)), dtype="float16")
-            labels_batch = np.empty(shape=(batch_size,seq_length), dtype="int32")
+            onehot_seq_batch = np.empty(
+                shape=(batch_size, seq_length, len(onehot_tok_idx)), dtype="float16")
+            labels_batch = np.empty(
+                shape=(batch_size, seq_length), dtype="int32")
 
             for batch_idx in range(0, batch_size):
 
-                for sent_idx in range(batch_idx*batch_size, (batch_idx+1)*batch_size):
+                for sent_idx in range(batch_idx * batch_size, (batch_idx + 1) * batch_size):
                     if sent_idx >= len(sent):
                         break
 
@@ -107,14 +128,15 @@ def data_iterator(file_path, onehot_tok_idx, num_batches, batch_size, seq_length
 
                         if tok not in onehot_tok_idx:
                             tok = "UNKNOWNTEXT"
-                        
-                        onehot_seq_batch[batch_idx][tok_idx][onehot_tok_idx[tok]] = 1
+
+                        onehot_seq_batch[batch_idx][
+                            tok_idx][onehot_tok_idx[tok]] = 1
                         labels_batch[batch_idx][tok_idx] = onehot_tok_idx[tok]
 
-                    yield onehot_seq_batch,labels_batch
+                    yield onehot_seq_batch, labels_batch
 
 if __name__ == "__main__":
-    
+
     en_file_path = "data/english_subtitles.gz"
     fr_file_path = "data/french_subtitles.gz"
 
@@ -133,12 +155,12 @@ if __name__ == "__main__":
 
 #    read_data(en_file_path, en_write_path, num_batches, min_count)
 
-
     print("processing french subtitles")
 
     if not os.path.isfile(fr_file_path):
-        urllib.request.urlretrieve("http://opus.lingfil.uu.se/download.php?f=OpenSubtitles2016/mono/OpenSubtitles2016.raw.fr.gz", filename=fr_file_path)
-        
+        urllib.request.urlretrieve(
+            "http://opus.lingfil.uu.se/download.php?f=OpenSubtitles2016/mono/OpenSubtitles2016.raw.fr.gz", filename=fr_file_path)
+
     print("...subtitles downloaded")
 
     read_data(fr_file_path, fr_write_path, num_batches, min_count)
--- lstm_autoencoder.py
+++ lstm_autoencoder.py
@@ -3,7 +3,7 @@ import numpy as np
 from data_preprocess import data_iterator
 
 # english
-# onehot_tok_idx = np.load('data/en_onehot')
+# onehot_tok_idx = np.load('data/en_onehot.npy')
 # en_file_path = "data/english_subtitles.gz"
 
 
@@ -12,8 +12,6 @@ onehot_tok_idx = np.load('data/fr_onehot.npy').item()
 fr_file_path = "data/french_subtitles.gz"
 
 # Build LSTM graph
-
-
 def length(sequence):
     used = tf.sign(tf.reduce_max(tf.abs(sequence), reduction_indices=2))
     length = tf.reduce_sum(used, reduction_indices=1)
@@ -22,37 +20,38 @@ def length(sequence):
 
 vocab_size = len(onehot_tok_idx)
 num_layers = 4
-num_steps = 40
-batch_size = 10
+num_steps = 20
+batch_size = 20
 hidden_size = 200
 
-# batch_size x num_steps x vocab_size with post-padding
+# tensor of shape [ batch_size x num_steps x vocab_size ] with post-padding
 encoder_inputs = tf.placeholder(tf.float16, shape=(
     batch_size, num_steps, vocab_size), name="placeholder_encoder_inputs")
 
+# tensor of shape [ batch_size x num_steps ]
+mask = tf.sign(tf.reduce_max(tf.abs(encoder_inputs), axis=2))
 
-# decoder_inputs must be a list of 2D tensors [batch_size x
-# vocab_size] of length num_steps
+# tensor of shape [ batch_size ]
+encoder_lengths = tf.cast(tf.reduce_sum(
+    mask, reduction_indices=1), tf.int32)
+
+# list of 2D tensors [ batch_size x vocab_size ] of length num_steps
 decoder_inputs = tf.unstack(tf.transpose(encoder_inputs, [1, 0, 2]), axis=0)
 decoder_inputs = (
     [tf.zeros_like(decoder_inputs[0], name="GO")] + decoder_inputs[:-1])
 
-# labels must be a list of 1D tensors [batch_size] of length num_steps
-raw_labels = tf.placeholder(tf.int32, shape=(batch_size, num_steps), name="placeholder_raw_labels")
-labels = tf.unstack(raw_labels, axis=1)
+decoder_weights = tf.Variable(tf.truncated_normal(
+    [hidden_size, vocab_size], stddev=0.05, dtype=tf.float16))
+decoder_bias = tf.Variable(
+    tf.constant(.1, shape=[vocab_size], dtype=tf.float16))
 
-# tensor of shape: [ batch_size x num_steps ]
-used_frames = tf.sign(tf.reduce_max(tf.abs(encoder_inputs), axis=2))
-# tensor of shape: [ batch_size ]
-encoder_sequence_lengths = tf.cast(tf.reduce_sum(
-    used_frames, reduction_indices=1), tf.int32)
-# list of 1D tensors of shape: [ batch_size ] of length num_steps
-loss_weights = tf.unstack(tf.cast(used_frames, tf.float16), axis=1)
+# list of 1D tensors [ batch_size ] of length num_steps
+labels = tf.placeholder(tf.int32, shape=(
+    batch_size, num_steps), name="placeholder_raw_labels")
+labels = tf.unstack(labels, axis=1)
 
-decoder_weights = tf.Variable(tf.truncated_normal([hidden_size, vocab_size], stddev=0.05, dtype=tf.float16))
-decoder_bias = tf.Variable(tf.constant(.1, shape=[vocab_size], dtype=tf.float16))
-
-# labels = encoder_inputs
+# list of 1D tensors [ batch_size ] of length num_steps
+loss_weights = tf.unstack(tf.cast(mask, tf.float16), axis=1)
 
 lstm = tf.contrib.rnn.BasicLSTMCell(
     hidden_size, forget_bias=0.0, state_is_tuple=True)
@@ -62,21 +61,23 @@ stacked_lstm = tf.contrib.rnn.MultiRNNCell(
 initial_state = stacked_lstm.zero_state(batch_size, dtype=tf.float16)
 
 encoder_outputs, encoder_state = tf.nn.dynamic_rnn(
-   cell=stacked_lstm, inputs=encoder_inputs, initial_state=initial_state, dtype=tf.float16, sequence_length=encoder_sequence_lengths)
+    cell=stacked_lstm, inputs=encoder_inputs, initial_state=initial_state, dtype=tf.float16, sequence_length=encoder_lengths)
 
 decoder_outputs, decoder_state = tf.contrib.legacy_seq2seq.rnn_decoder(
     decoder_inputs=decoder_inputs, initial_state=encoder_state, cell=stacked_lstm)
 
-preds = [tf.matmul(step, decoder_weights) + decoder_bias for step in decoder_outputs]
+preds = [tf.matmul(step, decoder_weights) +
+         decoder_bias for step in decoder_outputs]
 
-loss = tf.contrib.legacy_seq2seq.sequence_loss(logits=preds, targets=labels, weights=loss_weights)
+loss = tf.contrib.legacy_seq2seq.sequence_loss(
+    logits=preds, targets=labels, weights=loss_weights)
 
-optimizer = tf.train.AdamOptimizer(1e-4)
+optimizer = tf.train.AdamOptimizer(1e-5)
 train_op = optimizer.minimize(loss)
 
 print("graph loaded")
 
-iter_ = data_iterator(fr_file_path, onehot_tok_idx, 1,batch_size, num_steps)
+iter_ = data_iterator(fr_file_path, onehot_tok_idx, 1, batch_size, num_steps)
 
 print("iterator loaded")
 
@@ -89,11 +90,12 @@ print("variables initialized")
 with tf.Session() as sess:
     sess.run(init)
     for i in range(200000):
-        sequences_batch,labels_batch = iter_.__next__()
+        sequences_batch, labels_batch = iter_.__next__()
 
         if (i + 1) % 100 == 0:
             train_accuracy = loss.eval(session=sess, feed_dict={
-                                       encoder_inputs: sequences_batch, raw_labels: labels_batch})
+                                       encoder_inputs: sequences_batch, labels: labels_batch})
             print("step %d, training loss %g" % (i + 1, train_accuracy))
 
-        train_op.run(session=sess, feed_dict={encoder_inputs: sequences_batch, raw_labels: labels_batch})
+        train_op.run(session=sess, feed_dict={
+                     encoder_inputs: sequences_batch, labels: labels_batch})
