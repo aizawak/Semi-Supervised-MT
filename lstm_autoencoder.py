a59be1835a812a680f2fe762bc24cdfac60db527
Ken Aizawa <k.aizawa@live.com>
2017-03-01 02:23:09 +0000

    add mat-mul + clean

--- lstm_autoencoder.py
+++ lstm_autoencoder.py
@@ -24,18 +24,16 @@ vocab_size = len(onehot_tok_idx)
 num_layers = 4
 num_steps = 40
 batch_size = 10
-hidden_size = 1000
+hidden_size = 200
 
 # batch_size x num_steps x vocab_size with post-padding
-raw_sequence = tf.placeholder(tf.float16, shape=(
-    batch_size, num_steps, vocab_size), name="placeholder_raw_sequence")
+encoder_inputs = tf.placeholder(tf.float16, shape=(
+    batch_size, num_steps, vocab_size), name="placeholder_encoder_inputs")
 
-# encoder_inputs must be a 3D tensor [num_steps x batch_size x vocab_size]
-encoder_inputs = tf.transpose(raw_sequence, [1, 0, 2])
 
 # decoder_inputs must be a list of 2D tensors [batch_size x
 # vocab_size] of length num_steps
-decoder_inputs = tf.unstack(encoder_inputs, axis=0)
+decoder_inputs = tf.unstack(tf.transpose(encoder_inputs, [1, 0, 2]), axis=0)
 decoder_inputs = (
     [tf.zeros_like(decoder_inputs[0], name="GO")] + decoder_inputs[:-1])
 
@@ -43,22 +41,16 @@ decoder_inputs = (
 raw_labels = tf.placeholder(tf.int32, shape=(batch_size, num_steps), name="placeholder_raw_labels")
 labels = tf.unstack(raw_labels, axis=1)
 
-# tf.reduce_max() collapses one-hot dimension to max (1 if used step or 0 if unused step)
-# tf.sign() to convert max value to 1
 # tensor of shape: [ batch_size x num_steps ]
-used_frames = tf.sign(tf.reduce_max(tf.abs(raw_sequence), axis=2))
-
-# tf.reduce_sum() collapses indicator dimension (1 if used step or 0 if unused step) by summing values
-# tf.cast() to convert to type tf.int32
+used_frames = tf.sign(tf.reduce_max(tf.abs(encoder_inputs), axis=2))
 # tensor of shape: [ batch_size ]
 encoder_sequence_lengths = tf.cast(tf.reduce_sum(
     used_frames, reduction_indices=1), tf.int32)
-
-# tf.transpose() takes transpose
-# tf.cast() to convert to type tf.float16
-# tf.unstack() to create list of 1D tensors
 # list of 1D tensors of shape: [ batch_size ] of length num_steps
-decoder_weights = tf.unstack(tf.cast(used_frames, tf.float16), axis=1)
+loss_weights = tf.unstack(tf.cast(used_frames, tf.float16), axis=1)
+
+decoder_weights = tf.Variable(tf.truncated_normal([hidden_size, vocab_size], stddev=0.05, dtype=tf.float16))
+decoder_bias = tf.Variable(tf.constant(.1, shape=[vocab_size], dtype=tf.float16))
 
 # labels = encoder_inputs
 
@@ -70,12 +62,14 @@ stacked_lstm = tf.contrib.rnn.MultiRNNCell(
 initial_state = stacked_lstm.zero_state(batch_size, dtype=tf.float16)
 
 encoder_outputs, encoder_state = tf.nn.dynamic_rnn(
-    stacked_lstm, raw_sequence, initial_state=initial_state, dtype=tf.float16, sequence_length=encoder_sequence_lengths)
+   cell=stacked_lstm, inputs=encoder_inputs, initial_state=initial_state, dtype=tf.float16, sequence_length=encoder_sequence_lengths)
 
-outputs, state = tf.contrib.legacy_seq2seq.rnn_decoder(
+decoder_outputs, decoder_state = tf.contrib.legacy_seq2seq.rnn_decoder(
     decoder_inputs=decoder_inputs, initial_state=encoder_state, cell=stacked_lstm)
 
-loss = tf.contrib.legacy_seq2seq.sequence_loss(logits=outputs, targets=labels, weights=decoder_weights)
+preds = [tf.matmul(step, decoder_weights) + decoder_bias for step in decoder_outputs]
+
+loss = tf.contrib.legacy_seq2seq.sequence_loss(logits=preds, targets=labels, weights=loss_weights)
 
 optimizer = tf.train.AdamOptimizer(1e-4)
 train_op = optimizer.minimize(loss)
@@ -99,7 +93,7 @@ with tf.Session() as sess:
 
         if (i + 1) % 100 == 0:
             train_accuracy = loss.eval(session=sess, feed_dict={
-                                       raw_sequence: sequences_batch, raw_labels: labels_batch})
+                                       encoder_inputs: sequences_batch, raw_labels: labels_batch})
             print("step %d, training loss %g" % (i + 1, train_accuracy))
 
-        train_op.run(session=sess, feed_dict={raw_sequence: sequences_batch, raw_labels: labels_batch})
+        train_op.run(session=sess, feed_dict={encoder_inputs: sequences_batch, raw_labels: labels_batch})
