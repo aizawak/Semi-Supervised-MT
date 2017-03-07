8ead24f74434aab086df572d3ea58710e9e2d4ce
Ken Aizawa <k.aizawa@live.com>
2017-03-07 20:49:50 +0000

    generate validation

--- data_preprocess.py
+++ data_preprocess.py
@@ -74,6 +74,22 @@ def read_data(file_paths, write_path, num_batches, top_n):
     np.save(write_path, onehot_tok_idx)
     print("...token id's saved")
 
+def generate_val(file_path, write_path, num_val):
+
+    with gzip.open(file_path, 'rb') as f:
+        content = f.read().decode('utf-8').split('\n')
+
+    np.random.seed(seed=1)
+
+    np.random.shuffle(content)
+
+    raw = bytearray("\n".join(content[0:num_val]), "utf-8")
+
+    with gzip.open(write_path, 'wb') as wf:
+        wf.write(raw)
+    
+    print("...validation set saved")
+
 # Loop through batches and generate one-hot encodings of sequences and
 # sequence labels.
 
@@ -143,6 +159,15 @@ if __name__ == "__main__":
     fr_file_paths = ["data/fr_%d.gz" %
                      i for i in range(0, len(fr_file_sources))]
 
+    en_val_file_source = "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2007.en.shuffled.gz"
+    fr_val_file_source = "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2007.fr.shuffled.gz"
+
+    en_val_file_path = "en_val.gz"
+    fr_val_file_path = "fr_val.gz"
+
+    en_val_write_path = "en_val.gz"
+    fr_val_write_path = "fr_val.gz"
+
     en_write_path = "data/en_onehot.npy"
     fr_write_path = "data/fr_onehot.npy"
 
@@ -159,10 +184,16 @@ if __name__ == "__main__":
             urllib.request.urlretrieve(
                 file_source, filename=file_path)
 
+    if not os.path.isfile(en_val_file_path):
+        urllib.request.urlretrieve(
+            en_val_file_source, filename=en_val_file_path)    
+
     print("...files downloaded")
 
     read_data(file_paths=en_file_paths, write_path=en_write_path,
               num_batches=1, top_n=top_n)
+
+    generate_val(en_val_file_path, en_val_write_path, 3000)
 
     print("processing french files")
 
@@ -174,6 +205,13 @@ if __name__ == "__main__":
         if not os.path.isfile(file_source):
             urllib.request.urlretrieve(
                 file_source, filename=file_path)
+
+    if not os.path.isfile(fr_val_file_path):
+        urllib.request.urlretrieve(
+            fr_val_file_source, filename=fr_val_file_path)    
+
     print("...files downloaded")
 
     read_data(file_paths = fr_file_paths, write_path = fr_write_path, num_batches = 1, top_n = top_n)
+
+    generate_val(fr_val_file_path, fr_val_write_path, 3000)
